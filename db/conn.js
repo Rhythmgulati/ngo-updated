c4a213ea0386bac344b2ee87cd0991a6d752af41
jkkj <rhythmgulati11@gmail.com>
2024-05-06 15:56:09 +0530

    vercel db updated

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://rhythmgulati11:slSz0Uza9u9N0hju@cluster0.yuwvoas.mongodb.net/").then(()=>console.log("Db connected")).catch(()=>console.log("db connection error"));
+mongoose.connect("mongodb+srv://rhythmgulati11:6MBycbYcgt6Zr1V5@cluster0.yuwvoas.mongodb.net/").then(()=>console.log("Db connected")).catch(()=>console.log("db connection error"));
 
